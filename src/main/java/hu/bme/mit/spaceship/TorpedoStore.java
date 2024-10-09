3e91b999cd6f300755e9eb1f59e8901ab4c9d43b
hiroaki <nayuwqk@outlook.jp>
2024-10-09 13:49:38 +0200

    bugfix 2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -13,7 +13,6 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
-
   Random generator = new Random();
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -35,7 +34,7 @@ public class TorpedoStore {
     }
 
     boolean success = false;
-    Random generator = new Random();
+
     // simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
 
